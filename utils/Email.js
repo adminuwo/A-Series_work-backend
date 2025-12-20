b07535009e29efdea4609621d086985d8746a5ef
gurumukhahuja-ux <gurumukhahura@icloud.com>
2025-12-20 16:54:46 +0530

    added email new Template

--- utils/Email.js
+++ utils/Email.js
@@ -1,30 +1,27 @@
 import { Verification_Email_Template, Welcome_Email_Template } from "./EmailTemplate.js";
 import { resend, transporter } from "./Email.config.js";
 //  console.log(transporter);
- 
-export const sendVerificationEmail=async(email,verificationCode)=>{
-    try {
-     const response=   await resend.emails.send({
-            from:`AI-MALL <${process.env.EMAIL}>`,
-            to: [email], 
-            subject: "Verify Your Email", 
-            html: Verification_Email_Template.replace("{verificationCode}",verificationCode)
-        })
-        console.log('Email send Successfully',response)
-    } catch (error) {
-        console.log('Email error',error)
-    }
+
+export const sendVerificationEmail = async (email, name, verificationCode) => {
+  try {
+    const response = await resend.emails.send({
+      from: `AI-MALL <${process.env.EMAIL}>`,
+      to: [email],
+      subject: "Verify Your Email",
+      html: Verification_Email_Template.replace("{name}", name).replace("{verificationCode}", verificationCode)
+    })
+  } catch (error) {
+    console.log('Email error', error)
+  }
 }
 
 // WELCOME EMAIL
-export const welcomeEmail = async (name,email) => {
-  const info =await resend.emails.send({
+export const welcomeEmail = async (name, email) => {
+  const info = await resend.emails.send({
     from: `AI-MALL <${process.env.EMAIL}>`,
     to: [email],
     subject: `Welcome ${name}`,
-    html: Welcome_Email_Template.replace("{name}",name), 
+    html: Welcome_Email_Template.replace("{name}", name),
   });
-
-  console.log("Message sent:", info);
 };
 
